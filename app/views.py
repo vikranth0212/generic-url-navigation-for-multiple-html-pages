62a1564058a1573768f8cb88177493ea0ad88800
vikranth0212 <vikranth15.6.1995@gmail.com>
2023-11-20 18:54:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sudheer(request):
+    return render(request,'sudheer.html')
+
+def srujana(request):
+    return render(request,'srujana.html') 
+
+def sudheer2(request):
+    return render(request,'sudheer2.html')       
